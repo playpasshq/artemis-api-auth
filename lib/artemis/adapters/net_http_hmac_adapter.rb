0e9d7e3714160515e3c4804dc8d371f8376e7419
dale <dale@valid.be>
2025-12-01 19:16:13 +0100

    rubocop

--- lib/artemis/adapters/net_http_hmac_adapter.rb
+++ lib/artemis/adapters/net_http_hmac_adapter.rb
@@ -36,9 +36,7 @@ module Artemis
       def api_auth_sign_request!(request, api_auth)
         override_http_method = api_auth.fetch(:override_http_method, nil)
 
-        if api_auth.fetch(:add_content_md5, false)
-          request.headers['Content-MD5'] = Digest::MD5.base64digest(request.body)
-        end
+        request.headers['Content-MD5'] = Digest::MD5.base64digest(request.body) if api_auth.fetch(:add_content_md5, false)
 
         digest = api_auth.fetch(:digest, 'sha256')
         ::ApiAuth.sign!(
